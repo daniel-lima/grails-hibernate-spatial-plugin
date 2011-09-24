c46ba7371fe93d4b0ef6b29505cae81975ef2149
Daniel Henrique Alves Lima <email_daniel_h@yahoo.com.br>
2011-09-24 17:16:24 -0300

    Initial structure.

--- hibernate-spatial-mysql/HibernateSpatialMysqlGrailsPlugin.groovy
+++ hibernate-spatial-mysql/HibernateSpatialMysqlGrailsPlugin.groovy
@@ -1,19 +1,20 @@
 class HibernateSpatialMysqlGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+        'grails-app/scripts/**/Eclipse.groovy'
+            'grails-app/views/**/*'
     ]
 
     // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
+    def author = 'Daniel Henrique Alves Lima'
+    def authorEmail = 'email_daniel_h@yahoo.com.br'
+    def title = 'Grails Hibernate Spatial MySQL Plugin'
     def description = '''\\
 Brief description of the plugin.
 '''
